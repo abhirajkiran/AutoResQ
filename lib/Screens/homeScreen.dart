7e03b23491c24e099f68b598da4668b820d97fe2
Abhiraj Kiran <abhirajkiran100@gmail.com>
2023-04-20 23:53:00 +0530

    homescreen added without alignment

---
+++ lib/Screens/homeScreen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                Spacer(),
+                IconButton(onPressed: () {}, icon: Icon(Icons.notifications))
+              ],
+            ),
+            Column(
+              children: [Text('Hello'), Text('How can we help?')],
+            ),
+            SizedBox(height: 80),
+            Container(
+              height:100,
+              width: 300,
+              color: Colors.green,
+            ),
+
+            SizedBox(height: 50,),
+
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:60),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 120,
+                        color: Colors.green,
+                      ),
+                      SizedBox(width:30,),
+                      Container(
+                        height: 120,
+                        width: 120,
+                        color: Colors.green,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                 Padding(
+                  padding: const EdgeInsets.only(left:60),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 120,
+                        color: Colors.green,
+                      ),
+                      SizedBox(width:30,),
+                      Container(
+                        height: 120,
+                        width: 120,
+                        color: Colors.green,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Services',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions_car),
+              label: 'My Vehicles',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resp/Screens/homeScreen.dart';
 import 'package:resp/Screens/splashScreen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
