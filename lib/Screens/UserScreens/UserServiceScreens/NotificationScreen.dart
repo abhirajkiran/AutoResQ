21acab70261fdcda7ae744983b56df15247b7ada
Bhavith P <bhavithp2002@gmail.com>
2023-06-13 21:02:22 +0530

    notification screen added

--- lib/Screens/UserScreens/UserServiceScreens/NotificationScreen.dart
+++ lib/Screens/UserScreens/UserServiceScreens/NotificationScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resp/Screens/UserScreens/HomeScreens/homeScreen.dart';
+
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
 
@@ -12,21 +13,30 @@ class _NotificationScreen extends State<NotificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Notifications",style:TextStyle(fontSize: 22)),
+        title: Text("Notifications", style: TextStyle(fontSize: 22)),
         leading: IconButton(
-          onPressed: (){
+          onPressed: () {
             Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                );
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
           },
           icon: Icon(
             Icons.arrow_back,
             color: Colors.white,
           ),
         ),
-      )
-  
-      );
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Notification ${index + 1}'),
+            subtitle: Text('This is notification number ${index + 1}'),
+          );
+        },
+      ),
+    );
   }
 }
